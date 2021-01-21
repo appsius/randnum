a0c0a999838f27ad49ac6cd60bc793014a9a4d8b
apsius <38158216+appsius@users.noreply.github.com>
2021-01-21 19:41:59 +0300

    implementing highscores

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 'use strict';
 
-const secretNumber = Math.floor(Math.random() * 20) + 1;
+let secretNumber = Math.floor(Math.random() * 20) + 1;
 let score = 20;
+let highscore = 0;
 
-document.querySelector('.number').textContent = secretNumber;
+//document.querySelector('.guess').value = '';
 
 document.querySelector('.check').addEventListener('click', function () {
 	const guess = Number(document.querySelector('.guess').value);
@@ -11,10 +12,16 @@ document.querySelector('.check').addEventListener('click', function () {
 	if (!guess) {
 		document.querySelector('.message').textContent = '🚫 No Number!';
 	} else if (guess === secretNumber) {
-		document.querySelector('body').style.backgroundColor = '#60b347';
-		document.querySelector('.number').style.width = '30rem';
 		document.querySelector('.message').textContent =
 			'🏆🏆 Number is CORRECT 🏆🏆';
+		document.querySelector('.number').textContent = secretNumber;
+		document.querySelector('body').style.backgroundColor = '#60b347';
+		document.querySelector('.number').style.width = '30rem';
+
+		if (score > highscore) {
+			highscore = score;
+			document.querySelector('.highscore').textContent = highscore;
+		}
 	} else if (guess > secretNumber) {
 		if (score > 1) {
 			document.querySelector('.message').textContent =
@@ -38,4 +45,17 @@ document.querySelector('.check').addEventListener('click', function () {
 			document.querySelector('.score').textContent = 0;
 		}
 	}
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+	score = 20;
+	secretNumber = Math.floor(Math.random() * 20) + 1;
+
+	document.querySelector('.guess').value = '';
+	document.querySelector('.score').textContent = score;
+	document.querySelector('.message').textContent = 'Start guessing...';
+	document.querySelector('.number').textContent = '?';
+
+	document.querySelector('body').style.backgroundColor = '#222';
+	document.querySelector('.number').style.width = '15rem';
 });
